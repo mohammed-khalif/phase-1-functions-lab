98fac1b379f2517c2892ddac53113a702c0f99c6
mohammed-khalif <mohamed.yusuf@student.moringaschool.com>
2023-03-18 23:09:33 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(block) {
+    if (block <= 42) {
+      return 42 - block;
+    } else {
+      return block - 42;
+    }
+  }
+  
+  function distanceFromHqInFeet(block) {
+    return distanceFromHqInBlocks(block) * 264;
+  }
+  
+  function distanceTravelledInFeet(start, end) {
+    if (start < end) {
+      return (end - start) * 264;
+    } else {
+      return (start - end) * 264;
+    }
+  }
+  
+  function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+    
+    if (distance <= 400) {
+      return 0;
+    } else if (distance <= 2000) {
+      return (distance - 400) * 0.02;
+    } else if (distance <= 2500) {
+      return 25;
+    } else {
+      return "cannot travel that far";
+    }
+  }
+  
